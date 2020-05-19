824853b6769b30ce17ec63388b52e9e2f87f81b8
mikershov94 <ershovme@gmail.com>
2020-05-19 13:27:37 +0800

    Created files for webpack-config. In common added rules pug, css, sass

---
+++ config/webpack.common.conf.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const fs = require('fs');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const PATHS = {
+    src: path.join(__dirname, '../src'),
+    dist: path.join(__dirname, '../dist'),
+    test: path.join(__dirname, '../test'),
+    testDist: path.join(__dirname, '../test-dist')
+};
+
+const PAGE_DIR = `${PATHS.src}/pages`;
+const PAGES = fs.readdirSync(PAGE_DIR).filter(page => page.endsWith('.pug'));
+
+module.exports = {
+
+    entry: {
+        app: PATHS.src + '/index.js'
+    },
+    output: {
+        filename: 'js/app-[hash:7].js',
+        path: PATHS.dist
+    },
+
+    module: {
+        rules: [
+
+            {
+                test: /\.pug$/,
+                use: [
+                    { loader: 'pug-loader' }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            config: { path: __dirname }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: { 
+                            sourceMap: true,
+                            config: { path: __dirname }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            },
+
+        ]
+    }
+}
