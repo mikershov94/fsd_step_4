e7705a952e14e940895275f0e885b169fdf29c85
mikershov94 <ershovme@gmail.com>
2020-05-24 19:43:16 +0800

    Update model. Added prop position in constructor()

--- src/model.ts
+++ src/model.ts
@@ -1,4 +1,9 @@
 class Slider {
+    minLimit: number;
+    maxLimit: number;
+    step: number;
+    position: number;
+
     constructor(min: number = 0,
                 max: number = 100,
                 step: number = 0) {
@@ -6,6 +11,7 @@ class Slider {
         this.minLimit = min;
         this.maxLimit = max;
         this.step = step;
+        this.position = max / 2;
 
     }
 };
