2641ba6723d4794210fc0fe3c8321b7aa7998d64
mikershov94 <ershovme@gmail.com>
2020-05-28 14:51:36 +0800

    Update view.test. Added test for view.updatePosition()

--- tests/view.test.ts
+++ tests/view.test.ts
@@ -2,10 +2,11 @@ import SliderView from '../src/view';
 
 
 describe('Testing view.render()', () => {
-    const testTemplate = document;
-    testTemplate.body.innerHTML = '<div id="fsd-slider"></div>';
 
-    test('method render should set HTML-template to div#fsd-slider', () => {
+    test('view should set HTML-template to div#fsd-slider', () => {
+        const testTemplate = document;
+        testTemplate.body.innerHTML = '<div id="fsd-slider"></div>';
+
         const testView = new SliderView();
         //создаем ожидаемую HTML-разметку
         const resultTemplate = '<div class="wrapper">' +
@@ -13,13 +14,37 @@ describe('Testing view.render()', () => {
                                        '<div class="slider" data-position="50">' +
                                        '</div>' +
                                     '</div>' +
-                               '</div>'
+                               '</div>';
         
 
-        //вызываем тестируемый метод
+        //вызываем тестируемый метод c позицией 50
         testView.render(50);
 
         //проверяем результат его работы
         expect(testTemplate.body.children[0].innerHTML).toEqual(resultTemplate);
     });
+
+    test('view should update slider position in HTML', () => {
+        const testTemplate = document;
+        testTemplate.body.innerHTML = '<div id="fsd-slider"></div>';
+
+        const testView = new SliderView();
+        const sourceTemplate = '<div class="wrapper">' +
+                                    '<div class="rail">' +
+                                        '<div class="slider" data-position="50">' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>'
+        
+        const resultTemplate = '<div class="wrapper">' +
+                                    '<div class="rail">' +
+                                        '<div class="slider" data-position="25">' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>'
+
+        expect(testTemplate.body.children[0].innerHTML).toEqual(sourceTemplate);
+        testView.updatePosition(25)
+        expect(testTemplate.body.children[0].innerHTML).toEqual(resultTemplate);
+    })
 })
