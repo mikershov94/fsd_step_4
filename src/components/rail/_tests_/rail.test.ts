a9cc6b385254c077bf904809498ea0fbdebec055
mikershov94 <ershovme@gmail.com>
2020-06-22 13:09:09 +0800

    Created tests for rail

--- src/components/rail/_tests_/rail.test.ts
+++
@@ -1,9 +0,0 @@
-import rail from '../rail';
-
-describe('UI-component rail', () => {
-    
-    it('rail should return Jquery-method', () => {
-        expect(rail()).toBeTruthy;
-    })
-
-})
--- src/components/rail/rail.ts
+++ src/components/rail/rail.ts
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 const rail = (): JQuery => {
     return $('<div class="rail"></div>')
 };
