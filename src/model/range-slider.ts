058d92409e708079d8dd3d5fdf920d908f28933c
mikershov94 <ershovme@gmail.com>
2020-07-16 14:48:19 +0800

    Added RangeSliderModel

--- src/model/range-slider.ts
+++ src/model/range-slider.ts
@@ -1,4 +1,28 @@
-class RangeSliderModel {
+/// <reference path="model.d.ts" />
+
+class RangeSliderModel implements IRangeSliderModel {
+
+    public minLimit: number;
+    public maxLimit: number;
+
+    protected _positionA: number;
+    protected _positionB: number;
+
+    getPositionA(): number {
+        return this._positionA;
+    }
+
+    getPositionB(): number {
+        return this._positionB;
+    }
+
+    setPositionA(value: number): void {
+        this._positionA = value;
+    }
+
+    setPositionB(value: number): void {
+        this._positionB = value;
+    }
 
 }
 
