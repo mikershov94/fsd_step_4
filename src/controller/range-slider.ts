4b8ca4a08cae650698e5394753e88946613281a1
mikershov94 <ershovme@gmail.com>
2020-07-16 15:18:24 +0800

    Added RangeSliderController

--- src/controller/range-slider.ts
+++ src/controller/range-slider.ts
@@ -1,7 +1,15 @@
 /// <reference path="controller.d.ts" />
+/// <reference path="../model/model.d.ts" />
+/// <reference path="../view/view.d.ts" />
 
 class RangeSliderController implements IRangeSliderController {
+    public model: IRangeSliderModel;
+    public view: IRangeSliderView;
 
+    runPlugin(): void {
+        this.view.render();
+        this.view.initObserver();
+    }
 }
 
 export default RangeSliderController;
