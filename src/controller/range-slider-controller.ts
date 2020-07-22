b75a6a2528699a35170a52471256dc214fcf0723
mikershov94 <ershovme@gmail.com>
2020-07-22 15:40:27 +0800

    Update controller of range-slider. Event handlers added in property-object - actions

--- src/controller/range-slider-controller.ts
+++ src/controller/range-slider-controller.ts
@@ -6,56 +6,56 @@ class RangeSliderController implements IRangeSliderController {
     public model: IRangeSliderModel;
     public view: IRangeSliderView;
 
-    public onMouseDownA: IDownHandler;
-    public onMouseDownB: IDownHandler;
-    public onMouseMoveA: IMoveHandler;
-    public onMouseMoveB: IMoveHandler;
-    public onMouseUp: IUpHandler;
+    public actions: IRangeActions;
 
     constructor(model: IRangeSliderModel, view: IRangeSliderView) {
         this.model = model;
         this.view = view;
 
-        this.onMouseDownA = (e: JQuery.MouseDownEvent) => {
-            this.view.subscribeOnMoveA();
-            this.view.subscribeOnMouseUp();
-        }
-
-        this.onMouseDownB = (e: JQuery.MouseDownEvent) => {
-            this.view.subscribeOnMoveB();
-            this.view.subscribeOnMouseUp();
-        }
-
-        this.onMouseMoveA = (e: JQuery.MouseMoveEvent) => {
-            const posB: number = this.model.getPositionB();
-            let position: number = this.view.moveSliderA(e.pageX, this.model.minLimit, posB);
-            this.view.moveFillA(position, posB);
+        this.actions = {
             
-            this.model.setPositionA(Math.floor(position));
-            this.view.outputFieldA.val(this.model.getPositionA());
+            onMouseDownA: (e: JQuery.MouseDownEvent) => {
+                this.view.subscribeOnMoveA();
+                this.view.subscribeOnMouseUp();
+            },
+
+            onMouseDownB: (e: JQuery.MouseDownEvent) => {
+                this.view.subscribeOnMoveB();
+                this.view.subscribeOnMouseUp();
+            },
+
+            onMouseMoveA: (e: JQuery.MouseMoveEvent) => {
+                const posB: number = this.model.getPositionB();
+                let position: number = this.view.moveSliderA(e.pageX, this.model.minLimit, posB);
+                this.view.moveFillA(position, posB);
+                
+                this.model.setPositionA(Math.floor(position));
+                this.view.outputFieldA.val(this.model.getPositionA());
+            },
+
+            onMouseMoveB: (e: JQuery.MouseMoveEvent) => {
+                const posA: number = this.model.getPositionA();
+                let position: number = this.view.moveSliderB(e.pageX, this.model.maxLimit, posA);
+                this.view.moveFillB(posA, position);
+                
+                this.model.setPositionB(Math.floor(position));
+                this.view.outputFieldB.val(this.model.getPositionB());
+            },
+
+            onMouseUp: (e: JQuery.MouseUpEvent) => {
+                this.view.unsubscribeMouseMove();
+            }
         }
 
-        this.onMouseMoveB = (e: JQuery.MouseMoveEvent) => {
-            const posA: number = this.model.getPositionA();
-            let position: number = this.view.moveSliderB(e.pageX, this.model.maxLimit, posA);
-            this.view.moveFillB(posA, position);
-            
-            this.model.setPositionB(Math.floor(position));
-            this.view.outputFieldB.val(this.model.getPositionB());
-        }
-
-        this.onMouseUp = (e: JQuery.MouseUpEvent) => {
-            this.view.unsubscribeMouseMove();
-        }
     }
 
     runPlugin(defaultValues: IConfigPlugin): void {
         this.view.render(defaultValues.defaultPositionA, defaultValues.defaultPositionB);
-        this.view.initObserver(this.onMouseDownA,
-                               this.onMouseDownB,
-                               this.onMouseMoveA,
-                               this.onMouseMoveB,
-                               this.onMouseUp);
+        this.view.initObserver(this.actions.onMouseDownA,
+                               this.actions.onMouseDownB,
+                               this.actions.onMouseMoveA,
+                               this.actions.onMouseMoveB,
+                               this.actions.onMouseUp);
     }
 }
 
