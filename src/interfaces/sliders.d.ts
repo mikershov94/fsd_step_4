8722f3a0da9b35f69341157435f35e2bb6232f40
mikershov94 <ershovme@gmail.com>
2020-07-26 16:24:05 +0800

    Added slider types

---
+++ src/interfaces/sliders.d.ts
@@ -0,0 +1,19 @@
+interface TAloneSlider {
+    container: JQuery;
+    wrapper: JQuery;
+    rail: JQuery;
+    slider: JQuery;
+    progressBar: JQuery;
+    outputField: JQuery;
+}
+
+interface TRangeSlider {
+    container: JQuery;
+    wrapper: JQuery;
+    rail: JQuery;
+    sliderA: JQuery;
+    sliderB: JQuery;
+    progressBar: JQuery;
+    outputFieldA: JQuery;
+    outputFieldB: JQuery;
+}
