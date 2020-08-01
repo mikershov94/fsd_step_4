bdddd38bed8034b2815134aea5a362cf70292a24
mikershov94 <ershovme@gmail.com>
2020-08-01 18:58:27 +0800

    Update abstract Component. Added setTemplate()

--- src/view/components/index.ts
+++ src/view/components/index.ts
@@ -1,4 +1,4 @@
-abstract class Component {
+abstract class Component implements IComponent{
 
     protected state: TState;
     protected parent: any;
@@ -6,7 +6,15 @@ abstract class Component {
     protected template: string; //обязательное
 
     constructor(props: any) {
-        
+        this.template = this.setTemplate();
+    }
+
+    protected setTemplate(): string {
+        return '<div></div>';
+    }
+
+    render(): JQuery {
+        return $(this.template);
     }
 
 }
