f341c790bb876ad6dcdb50c4f0643fed2fb1221a
mikershov94 <ershovme@gmail.com>
2020-08-21 12:53:43 +0800

    Deleted old interfaces of model

--- src/interfaces/model.d.ts
+++ src/interfaces/model.d.ts
@@ -1,26 +1,3 @@
-interface IAloneSliderModel {
-    minLimit: number;
-    maxLimit: number;
-
-    getPosition(): number;
-    setPosition(value: number): void;
-
-    getStep(): number;
-    setStep(value: number): void;
-}
-
-interface IRangeSliderModel {
-    
-    minLimit: number;
-    maxLimit: number;
-
-    getPositionA(): number;
-    getPositionB(): number;
-    setPositionA(value: number): void;
-    setPositionB(value: number): void;
-
-}
-
 interface IAloneModelState {
     min: number,
     max: number,
