e6190fbe136331ffaca4b440d15883dde31b9e03
mikershov94 <ershovme@gmail.com>
2020-08-21 13:04:35 +0800

    Deleted old state-interfaces

--- src/interfaces/model.d.ts
+++ src/interfaces/model.d.ts
@@ -1,33 +1,3 @@
-interface IAloneModelState {
-    min: number,
-    max: number,
-    step: number,
-    defaultPosition: number,
-}
-
-interface IRangeModelState {
-    min: number,
-    max: number,
-    step: number,
-    defaultPositionA: number,
-    defaultPositionB: number
-}
-
-interface TGlobalState {
-    min: number;
-    max: number;
-    step: number;
-}
-
-interface TAloneGlobalState extends TGlobalState {
-    position: number;
-}
-
-interface TRangeGlobalState extends TGlobalState {
-    positionA: number;
-    positionB: number;
-}
-
 type ModelState = TGlobalState | TAloneGlobalState | TRangeGlobalState;
 
 interface IModel {
