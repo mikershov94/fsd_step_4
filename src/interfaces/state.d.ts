355b7a58b8a26b8f8dd20c2fd9dfd0eb9c0ed48c
mikershov94 <ershovme@gmail.com>
2020-08-21 13:03:58 +0800

    Move state-interfaces to state.d.ts

---
+++ src/interfaces/state.d.ts
@@ -0,0 +1,14 @@
+interface TGlobalState {
+    min: number;
+    max: number;
+    step: number;
+}
+
+interface TAloneGlobalState extends TGlobalState {
+    position: number;
+}
+
+interface TRangeGlobalState extends TGlobalState {
+    positionA: number;
+    positionB: number;
+}
