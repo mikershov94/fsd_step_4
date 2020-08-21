ffab95f3aaccd30ad709abe689ad9c0b46b80b22
mikershov94 <ershovme@gmail.com>
2020-08-21 14:08:55 +0800

    Created IMainView

--- src/interfaces/view.d.ts
+++ src/interfaces/view.d.ts
@@ -1,7 +1,10 @@
 interface IView {
+    update(message: TMessage): void;
+    render(): JQuery;
+}
+
+interface IMainView extends IView {
     page: JQuery<Document>;
     adopt(container: IComponent): IComponent;
     mountApplication(props: TMessage): void;
-    updateComponents(props: TMessage): void;
-    render(): JQuery;
 }
