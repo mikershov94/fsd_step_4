384264385179aebd67562418bdbe3a225b0b4edf
mikershov94 <ershovme@gmail.com>
2020-12-03 13:38:28 +0800

    Update progressBarFill component

--- src/components/progress-bar__fill/progress-bar__fill.ts
+++ src/components/progress-bar__fill/progress-bar__fill.ts
@@ -9,6 +9,7 @@ interface TProgressBarFillState extends TState {
     railLengthPx: number,
     vertical: boolean;
     prefix: string;
+    type: string;
 }
 
 class ProgressBarFill extends Component {
@@ -29,6 +30,7 @@ class ProgressBarFill extends Component {
             railLengthPx: this.props.railLengthPx,
             vertical: this.props.vertical,
             prefix: this.props.prefix,
+            type: this.props.type,
         }
     }
 
@@ -39,8 +41,8 @@ class ProgressBarFill extends Component {
     }
 
     protected doingRender(): void {
-        let startPercent: number = this.fillIn(this.state.start, this.state.max, this.state.min);
-        let endPercent: number = this.fillIn(this.state.end, this.state.max, this.state.min);
+        let startPercent: number = this.calcStart(this.state.start, this.state.max, this.state.min);
+        let endPercent: number = this.calcEnd(this.state.start, this.state.end, this.state.max, this.state.min);
 
         if (this.state.vertical) {
             this.jQueryElement.css('bottom', `${startPercent}%`);
@@ -54,17 +56,40 @@ class ProgressBarFill extends Component {
 
 
     protected updateState(): void {
-        this.setState({
-            start: this.props.start,
-            end: this.props.end,
-            sizeSlider: this.props.outerSizeSlider,
-            railLengthPx: this.props.lengthRail,
-        });
+        switch (this.state.type) {
+            case 'alone':
+                this.setState({
+                    start: this.props.min,
+                    end: this.props.value,
+                    sizeSlider: this.props.outerSizeSlider,
+                    railLengthPx: this.props.lengthRail,
+                });
+                return;
+
+            case 'range':
+                this.setState({
+                    start: this.props.valueA,
+                    end: this.props.valueB,
+                    sizeSlider: this.props.outerSizeSlider,
+                    railLengthPx: this.props.lengthRail,
+                });
+                return;
+
+            default:
+                this.setState({
+                    start: this.props.min,
+                    end: this.props.value,
+                    sizeSlider: this.props.outerSizeSlider,
+                    railLengthPx: this.props.lengthRail,
+                });
+                return; 
+
+        }
     }
 
     protected updateRender(): void {
-        let startPercent: number = this.fillIn(this.state.start, this.state.max, this.state.min);
-        let endPercent: number = this.fillIn(this.state.end, this.state.max, this.state.min);
+        let startPercent: number = this.calcStart(this.state.start, this.state.max, this.state.min);
+        let endPercent: number = this.calcEnd(startPercent, this.state.end, this.state.max, this.state.min);
 
         if (this.state.vertical) {
             this.jQueryElement.css('bottom', `${startPercent}%`);
@@ -76,10 +101,24 @@ class ProgressBarFill extends Component {
         this.jQueryElement.css('width', `${endPercent}%`);
     }
 
-    private fillIn(value: number, max: number, min: number): number {
+    private calcStart(value: number, max: number, min: number): number {
         const sizeSliderPercent: number = (this.state.sizeSlider * 100) / this.state.railLengthPx / 2;
 
         let fill: number = (value * 100) / (max - min);
+        
+        //fill = this.state.vertical ? fill - sizeSliderPercent : fill;
+        
+        //if (fill <= sizeSliderPercent) fill = sizeSliderPercent;
+        //if (fill >= 100) fill = 100;
+        
+        return fill;
+    }
+
+    private calcEnd(startPercent: number, end: number, max: number, min: number): number {
+        const sizeSliderPercent: number = (this.state.sizeSlider * 100) / this.state.railLengthPx / 2;
+
+        let fill: number = (end * 100) / (max - min) - startPercent;
+        console.log(fill)
         
         fill = this.state.vertical ? fill - sizeSliderPercent : fill + sizeSliderPercent;
         
